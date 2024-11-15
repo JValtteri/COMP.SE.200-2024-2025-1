74785220d6ee5241dd9a7cacffc3f67a021ff059
JValtteri <46611993+JValtteri@users.noreply.github.com>
2024-11-15 18:15:26 +0200

    Updated test coverage

--- test/eq.test.js
+++ test/eq.test.js
@@ -2,7 +2,16 @@ import { expect } from "chai";
 import eq from "../src/eq.js";
 
 describe("Eq", () => {
-  it("Test that 2 equals 2", () =>{
+  it("Test eq int", () =>{
     expect(eq(2,2)).to.equal(true)
+  });
+  it("Test eq string", () =>{
+    expect(eq('foo','foo')).to.equal(true)
+  });
+  it("Test obj to bool", () =>{
+    expect(eq({},false)).to.equal(false)
+  });
+  it("Test not 1=2", () =>{
+    expect(eq(1,2)).to.equal(false)
   });
 })

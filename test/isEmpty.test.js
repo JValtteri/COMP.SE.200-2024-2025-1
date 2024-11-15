fa128132c7536494b8a36069c2e3555c6ca6f629
JValtteri <46611993+JValtteri@users.noreply.github.com>
2024-11-15 18:55:33 +0200

    Inproved isEmpty test coverage

--- test/isEmpty.test.js
+++ test/isEmpty.test.js
@@ -4,6 +4,9 @@ import isEmpty from "../src/isEmpty.js";
 const test_input = { 'City': {'Street': {'House': {'Pets': ['cat', 'dog'] } } } };
 const test_path_1 = 'City.Street.House.Pets[1]'
 
+const func = isEmpty;
+
+//const proto = {}
 
 describe("Is empty", () => {
   it("null value", () =>{
@@ -11,5 +14,26 @@ describe("Is empty", () => {
   });
   it("Object", () =>{
     expect(isEmpty(test_input)).to.equal(false)
+  });
+  it("Empty list", () =>{
+    expect(isEmpty([])).to.equal(true)
+  });
+  it("Non-empty string", () =>{
+    expect(isEmpty('spam')).to.equal(false)
+  });
+  it("Non-empty map", () =>{
+    expect(isEmpty( {'foo': 'bar'} )).to.equal(false)
+  });
+//  it("Empty map", () =>{
+//    expect(isEmpty( {'foo': [] } )).to.equal(false)
+//  });
+  it("Function is empty", () =>{
+    expect(isEmpty( func )).to.equal(true)
+  });
+//  it("Protorype", () =>{
+//    expect(isEmpty( Object.create(proto) )).to.equal(true)
+//  });
+  it("Set", () =>{
+    expect(isEmpty( new Set(['a', 'b', 'c']) )).to.equal(false)
   });
 })

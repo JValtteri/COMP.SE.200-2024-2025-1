880e5763943b96fa1a21a5e711bd2e2c4623fe64
mparta <185134659+mparta@users.noreply.github.com>
2024-11-21 22:49:24 +0200

    Fixed slice.test to call the desired function correctly.

--- test/slice.test.js
+++ test/slice.test.js
@@ -9,13 +9,20 @@ describe("slice", () => {
 
     it("Slice of 2 should return elements 3,4,5 of array with 5 elements ", () =>{
         let targetArray = [3, 4, 5];
-        expect(testArrayInteger.slice(2)).to.deep.equal(targetArray)
+        expect(slice(testArrayInteger, 2)).to.deep.equal(targetArray)
     });
-    it("Slice of -3 should return elements 3,4,5 of array with 5 elements ", () =>{
+    it("Slice of -3 should return elements three, four ,five of array with 5 elements ", () =>{
         let targetArray = ["three", "four", "five"];
-        expect(testArrayString.slice(-3, 5)).to.deep.equal(targetArray)
+        expect(slice(testArrayString, -3, 5)).to.deep.equal(targetArray)
     });
-    it("Slice of empty array should return empty array", () =>{
-        expect(testArrayEmpty.slice(2)).to.deep.equal([])
+    it("Slice of empty array with invalid indices should return empty array", () =>{
+        expect(slice(testArrayEmpty, -7, 7)).to.deep.equal([])
+    });
+    it("Slice of 0 to -2 should return elements one, two, three of array with 5 elements ", () =>{
+        let targetArray = ["one", "two", "three"];
+        expect(slice(testArrayString, 0, -2)).to.deep.equal(targetArray)
+    });
+    it("Slice of null array with bad indices should return empty array", () =>{
+        expect(slice(null, null, undefined)).to.deep.equal([])
     });
 })

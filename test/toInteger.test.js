beb1d82681e85ca951204c029cd4e24bfb29fe92
mparta <185134659+mparta@users.noreply.github.com>
2024-11-21 20:45:22 +0200

    Amended the tests that check correct rounding for toInteger.test and commented out wrong type test.

--- test/toInteger.test.js
+++ test/toInteger.test.js
@@ -1,12 +1,13 @@
 import { expect } from "chai";
 import toInteger from "../src/toInteger.js";
 
+// The rounding rules work according to standards
 describe("toInteger", () => {
     it("Positive float 3.2 to rounded down integer 3", () =>{
       expect(toInteger(3.2)).to.equal(3)
     });
-    it("Positive float 4.6 to rounded up integer 5", () =>{
-        expect(toInteger(4.6)).to.equal(5)
+    it("Positive float 4.6 to rounded down integer 4", () =>{
+        expect(toInteger(4.6)).to.equal(4)
       });
     it("Positive string 127 to integer 127", () =>{
       expect(toInteger("127")).to.equal(127)
@@ -14,10 +15,12 @@ describe("toInteger", () => {
     it("Negative string -14 to integer -14", () =>{
         expect(toInteger("-14")).to.equal(-14)
       });
+    /*
     it("Trying to make a word an integer should throw an error", () => {
         expect(function () {toInteger("word")} ).to.throw(TypeError)
     });
-    it("Negative string float -235.7 to rounded negative integer -236", () =>{
-        expect(toInteger("-235.7")).to.equal(-236)
+    */
+    it("Negative string float -235.7 to rounded negative integer -235", () =>{
+        expect(toInteger("-235.7")).to.equal(-235)
     });
 })
